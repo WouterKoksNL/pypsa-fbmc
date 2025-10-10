814feb6572d5a9f6fe9334fae999d2caf7b3a99b
WouterKoksNL <wouterkoks135@live.nl>
2025-10-10 18:36:42 +0200

    add tests

---
+++ tests/fbmc/constraints/test_security_constraints.py
@@ -0,0 +1,46 @@
+
+import unittest
+import pandas as pd
+import numpy as np
+import pypsa
+from src.fbmc.constraints.security_constrained import add_security_constraints
+from src.fbmc.network_conversion import nodal_to_zonal
+
+class TestSecurityConstraints(unittest.TestCase):
+
+    def setUp(self):
+        # Create a sample zonal network for testing
+        self.zonal_net = pd.DataFrame({
+            'bus': ['bus1', 'bus2', 'bus3'],
+            'zone_name': ['Z1', 'Z2', 'Z3']
+        })
+        self.nodal_net = pypsa.examples.scigrid_de()
+
+        quadrant1 = (self.nodal_net.buses.y > 51) & (self.nodal_net.buses.x > 9)
+        quadrant2 = (self.nodal_net.buses.y > 51) & (self.nodal_net.buses.x < 9)
+        quadrant3 = (self.nodal_net.buses.y < 51) & (self.nodal_net.buses.x < 9)
+        quadrant4 = (self.nodal_net.buses.y < 51) & (self.nodal_net.buses.x > 9)
+
+        # find buses in each quadrant
+        buses_q1 = self.nodal_net.buses[quadrant1]
+        buses_q2 = self.nodal_net.buses[quadrant2]
+        buses_q3 = self.nodal_net.buses[quadrant3]
+        buses_q4 = self.nodal_net.buses[quadrant4]
+
+        self.nodal_net.buses.loc[buses_q1.index, 'zone_name'] = 'Z1'
+        self.nodal_net.buses.loc[buses_q2.index, 'zone_name'] = 'Z2'
+        self.nodal_net.buses.loc[buses_q3.index, 'zone_name'] = 'Z3'
+        self.nodal_net.buses.loc[buses_q4.index, 'zone_name'] = 'Z4'
+        # Load your network data into `net`
+        self.snapshots = self.nodal_net.snapshots
+        self.branch_outages = self.nodal_net.passive_branches().index
+
+        # Call the function
+        self.nodal_net.add('Generator', self.nodal_net.buses.index, bus=self.nodal_net.buses.index, p_nom=5000, marginal_cost=1000)
+        self.zonal_net = nodal_to_zonal(self.nodal_net)
+
+    def test_add_security_constraints(self):
+        # Test the add_security_constraints function
+        self.setUp()
+        result = add_security_constraints(self.nodal_net, self.zonal_net, self.snapshots, branch_outages=self.branch_outages)
+        # self.assertIsInstance(result, pd.DataFrame)
---
+++ tests/test_basic_results.py
@@ -0,0 +1,92 @@
+import pypsa
+import pandas as pd
+import unittest
+
+from src.fbmc.network_conversion import nodal_to_zonal
+from src.fbmc.main import FBMCConfig, run_fbmc
+from src.fbmc.parameters.gsk import GSKMethod
+from src.fbmc.market_prices import calculate_zonal_prices
+
+
+class TestFBMCResults(unittest.TestCase):
+    
+    def mock_config(self):
+        config = FBMCConfig()
+        config.reliability_margin_factor = 0.0
+        config.gsk_method = GSKMethod.ADJUSTABLE_CAP
+        config.add_security_constraints = False
+        return config
+
+    def setup_network(self):
+        nodal_net = pypsa.Network()
+        nodal_net.set_snapshots(['1', '2'])
+        nodal_net.add('Bus', ['A1', 'B1', 'B2'])
+        nodal_net.buses.loc[:, 'zone_name'] = ['A', 'B', 'B']
+        nodal_net.add('Line', 'A1-B1', bus0='B1', bus1='A1', x=1, s_nom=10)
+        nodal_net.add('Line', 'A2-B1', bus0='B2', bus1='A1', x=1, s_nom=10)
+        nodal_net.add('Line', 'A1-A2', bus0='B1', bus1='B2', x=1, s_nom=10)
+        
+        nodal_net.add('Generator', 'gen_A1', bus='A1', p_nom=12, marginal_cost=400, carrier="Wind")
+        nodal_net.add('Generator', 'gen_B1', bus='B1', p_nom=12, marginal_cost=100, carrier="CCGT")
+        nodal_net.add('Generator', 'gen_B2', bus='B2', p_nom=12, marginal_cost=200, carrier="Oil")
+        nodal_net.add('Load', 'load_A1', bus='A1', p_set=[15, 15])
+        return nodal_net
+    
+
+    def run_fbmc(self, nodal_net):
+        nodal_net.optimize(solver_name='gurobi')
+        zonal_net = nodal_to_zonal(nodal_net)
+        zonal_net.remove('Link', zonal_net.links.index)
+        config = FBMCConfig()
+
+        config.reliability_margin_factor = 0.0
+        config.gsk_method = GSKMethod.ADJUSTABLE_CAP
+        zonal_net.loads_t.p_set = zonal_net.loads_t.p_set * (18/15)
+
+        gsk = pd.DataFrame(0., index=zonal_net.buses.index, columns=nodal_net.buses.index)
+        gsk.loc['A', 'A1'] = 1.0
+        gsk.loc['B', 'B1'] = 0.8
+        gsk.loc['B', 'B2'] = 0.2
+        gsk.columns.name = "Bus"
+        gsk.index.name = "Zone"
+        gsk_dict = {snapshot: gsk.copy()
+            for snapshot in zonal_net.snapshots}
+
+        zonal_net, _, z_ptdf, ram = run_fbmc(nodal_net, zonal_net, config=config, gsk=gsk_dict)
+
+        return nodal_net, zonal_net, z_ptdf, ram
+    
+    def test_obj_value(self):
+        """Test that the objective value is as expected."""
+        nodal_net = self.setup_network()
+        nodal_net, zonal_net, _, _ = self.run_fbmc(nodal_net)
+        assert abs(zonal_net.model.objective.value - 5333.3333) < 1e-3
+
+    def test_line_direction_invariance(self):
+        """Reversing the direction of lines should not change the objective value.
+        An exception here can mean that minRAM is not calculated correctly or that the minRAM constraint is not enforced. """
+        nodal_net = self.setup_network()
+        nodal_net, zonal_net, _, _ = self.run_fbmc(nodal_net)
+        obj1 = zonal_net.model.objective.value
+
+        # Reverse line direction
+        nodal_net.lines.loc['A1-B1', ['bus0', 'bus1']] = ['A1', 'B1']
+        nodal_net.lines.loc['A2-B1', ['bus0', 'bus1']] = ['A1', 'B2']
+        # nodal_net.lines.loc['A1-A2', ['bus0', 'bus1']] = ['B1', 'B2']
+
+        nodal_net, zonal_net, _, _ = self.run_fbmc(nodal_net)
+        obj2 = zonal_net.model.objective.value
+
+        assert abs(obj1 - obj2) < 1e-6
+    
+    def test_price_calculation(self):
+        """Test that the price calculation runs without errors."""
+        nodal_net = self.setup_network()
+        nodal_net, zonal_net, z_ptdf, ram = self.run_fbmc(nodal_net)
+
+        prices = calculate_zonal_prices(zonal_net.buses.index, zonal_net.snapshots, z_ptdf, zonal_net.model)
+        assert prices is not None
+        assert not prices.isna().any().any()
+        assert abs(prices.loc['1', 'A'] - 400.) < 1e-3
+        assert abs(prices.loc['1', 'B'] - 200.) < 1e-3
+        
