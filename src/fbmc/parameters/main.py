45166ddbb59552831a6c5c6bcc6bf351e8d0f684
WouterKoksNL <wouterkoks135@live.nl>
2025-10-17 14:09:12 +0200

    pre-filter on CNECs

--- src/fbmc/parameters/main.py
+++ src/fbmc/parameters/main.py
@@ -3,19 +3,18 @@ import pandas as pd
 import numpy as np
 
 
-from .cne import cne_router
-from .flows import get_base_flows, calculate_ram
+from .cnec import cnec_router
+from .flows import calculate_ram
 from .gsk import calculate_gsk
 from .ptdf import calculate_zonal_ptdf, get_subnetwork_ptdf
 from ..config import FBMCConfig
-from .net_positions import calc_net_positions_sub_network
+from .base_case import calc_base_net_positions, get_base_flows
 
 
 def calculate_fbmc_parameters(
         sub_network: pypsa.SubNetwork,
-        gsk: dict,
+        gsk: dict[pd.Timestamp, pd.DataFrame] | pd.DataFrame,
         config: FBMCConfig = FBMCConfig(),
-        add_zptdf_np_term: bool = True,
         ) -> tuple[pd.DataFrame, dict[pd.Timestamp, pd.DataFrame], (dict | pd.DataFrame)]:
     """
     Calculate the Flow-Based Market Coupling (FBMC) parameters for a given power network basecase.
@@ -43,34 +42,28 @@ def calculate_fbmc_parameters(
     if isinstance(gsk, pd.DataFrame):
         gsk = {snapshot: gsk.copy() for snapshot in sub_network.snapshots}
 
+    cnes = cnec_router(sub_network, config)
+
     ptdf = get_subnetwork_ptdf(sub_network)
 
     z_ptdf = {}
     for snapshot, gsk_snapshot in gsk.items():
-        z_ptdf[snapshot] = calculate_zonal_ptdf(ptdf, gsk_snapshot)
+        z_ptdf[snapshot] = calculate_zonal_ptdf(ptdf, gsk_snapshot, cnes)
 
-
-    # Calculate RAM - this remains the same as it's already snapshot-based
-    net_positions_base_case = calc_net_positions_sub_network(sub_network)
-    base_flows = get_base_flows(sub_network)  # shape: (snapshots, branches)
-    upper_ram = calculate_ram(sub_network,
-                    zonal_ptdf_dict = z_ptdf, 
-                    base_flows = base_flows,
-                    min_ram = config.min_ram, 
                     reliability_margin_factor = config.reliability_margin_factor,
                     net_positions_base_case=net_positions_base_case,
                     add_zptdf_np_term=add_zptdf_np_term,
                     flow_direction=1)
     lower_ram =  -1 * calculate_ram(sub_network,
+    net_positions_base_case = calc_base_net_positions(sub_network, config.use_zero_base_flows_flag)
+    base_flows = get_base_flows(sub_network, config.use_zero_base_flows_flag)  # shape: (snapshots, branches)
+    upper_ram, lower_ram = calculate_ram(sub_network,
                     zonal_ptdf_dict = z_ptdf, 
                     base_flows = base_flows,
                     min_ram = config.min_ram, 
                     reliability_margin_factor = config.reliability_margin_factor,
                     net_positions_base_case=net_positions_base_case,
-                    add_zptdf_np_term=add_zptdf_np_term,
-                    flow_direction=-1)
-    upper_ram, z_ptdf = cne_router(sub_network, upper_ram, z_ptdf, config)
-    lower_ram, _ = cne_router(sub_network, lower_ram, z_ptdf, config)
+                    )
     return upper_ram, lower_ram, z_ptdf
 
     
