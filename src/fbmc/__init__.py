121554225651e2717d04e7c2726c64be8b93f118
WouterKoksNL <wouterkoks135@live.nl>
2025-10-20 16:18:14 +0200

    empty __init__

--- src/fbmc/__init__.py
+++ src/fbmc/__init__.py
@@ -1,8 +1,7 @@
 """FBMC constraint implementation module."""
 
 from .main import setup_fbmc_model
-from .market_prices import calculate_zonal_prices
+from ..post_processing.market_prices import calculate_zonal_prices
 __all__ = ['setup_fbmc_model',
            'run_fbmc',
-           'add_redispatch_constraints',
            'calculate_zonal_prices']
