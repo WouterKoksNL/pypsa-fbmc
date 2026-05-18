3914f52a132614efe8e994ce4dfd156e9490229d
WouterKoksNL <wouterkoks135@live.nl>
2026-05-18 11:52:23 +0200

    simplify main caller since complex scripts are under /scrips

--- main.py
+++ main.py
@@ -176,24 +176,24 @@ def main(
     
 
 if __name__ == "__main__":
-    rm_list = [0.0, 0.1, 0.2, 0.3]
-    for r in rm_list:
-        obj3 = main(
-            case_name=Cases.PYPSA_EUR_UA, 
-            config_overrides={
-                "gsk_strategy": GSKStrategy.P_NOM,
-                "base_case_strategy": BaseCaseStrategy.ZERO_FLOWS,
-                "advanced_hybrid_coupling_flag": False,
-                "reliability_margin_factor": r,
-                "add_security_constraints": False,
-                "security_constrained_redispatch": False,
-            },
-            load_case_flag=False,
-            case_kwargs={
-                'snapshot_i_range': slice(0, 24),
-                # 'drop_countries': ["UA"]
-            },
-        )  
+    config_path = Path("configs/base_config.yaml")
+    config = FBMCConfig().from_base_yaml(config_path)
+    obj3 = main(
+        case_name=Cases.PYPSA_EUR_UA, 
+        config_overrides={
+            "gsk_strategy": GSKStrategy.P_NOM,
+            "base_case_strategy": BaseCaseStrategy.ZERO_FLOWS,
+            "advanced_hybrid_coupling_flag": False,
+            "reliability_margin_factor": 0.3,
+            "add_security_constraints": False,
+            "security_constrained_redispatch": False,
+        },
+        load_case_flag=False,
+        case_kwargs={
+            'snapshot_i_range': slice(0, 2),
+            # 'drop_countries': ["UA"]
+        },
+    )  
 
 
 
