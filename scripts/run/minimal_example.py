c13ef36bf43cdc76c2d8450f7cdb326055cd7c19
WouterKoksNL <wouterkoks135@live.nl>
2026-05-18 16:15:06 +0200

    create minimal example

---
+++ scripts/run/minimal_example.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+from src.config import FBMCConfig
+from main import main
+from src.case_creation.main import Cases
+
+
+config_path = Path("config/base_config.yaml")
+config = FBMCConfig.from_base_yaml(config_path)
+obj = main(
+    case_name=Cases.BASIC_THREE_NODE, 
+    config=config,
+    load_case_flag=False,
+)  
+print("Zonal market objective: ", obj)
