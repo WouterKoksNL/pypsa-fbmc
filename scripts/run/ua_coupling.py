db0134deb39204dccd6b5b90f82ad47935991a32
WouterKoksNL <wouterkoks135@live.nl>
2026-05-20 10:21:18 +0200

    prepare for real run

--- scripts/run/ua_coupling.py
+++ scripts/run/ua_coupling.py
@@ -10,31 +10,31 @@ from src.enums import GSKStrategy, BaseCaseStrategy
 from main import main
 from src.paths import get_case_results_dir, get_results_dir, get_input_networks_dir
 
-# config_path = Path("config/base_config.yaml")
-# config = FBMCConfig.from_base_yaml(config_path)
-# save_path = Path(get_case_results_dir(Cases.PYPSA_EUR_UA.value))  / "base"
-# obj3 = main(
-#     save_path=save_path,
-#     case_name=Cases.PYPSA_EUR_UA, 
-#     config=config,
-#     config_overrides={
-#         "gsk_strategy": GSKStrategy.P_NOM,
-#         "base_case_strategy": BaseCaseStrategy.ZERO_FLOWS,
-#         "advanced_hybrid_coupling_flag": False,
-#         "reliability_margin_factor": 0.3,
-#         "add_security_constraints": False,
-#         "security_constrained_redispatch": False,
-#     },
-#     load_case_flag=False,
-#     case_kwargs={
-#         # 'drop_countries': ["UA"]
-#     },
-#     case_alteration_kwargs={
-#         'snapshot_i_range': slice(0, 2),
-#         'use_unit_commitment': False,
-#         'unit_commitment_path': "data/unit_commitment_halve_su_sd.csv",
-#     }
-# )  
+config_path = Path("config/base_config.yaml")
+config = FBMCConfig.from_base_yaml(config_path)
+save_path = Path(get_case_results_dir(Cases.PYPSA_EUR_UA.value))  / "base"
+obj3 = main(
+    save_path=save_path,
+    case_name=Cases.PYPSA_EUR_UA, 
+    config=config,
+    config_overrides={
+        "gsk_strategy": GSKStrategy.P_NOM,
+        "base_case_strategy": BaseCaseStrategy.ZERO_FLOWS,
+        "advanced_hybrid_coupling_flag": False,
+        "reliability_margin_factor": 0.3,
+        "add_security_constraints": False,
+        "security_constrained_redispatch": False,
+    },
+    load_case_flag=False,
+    case_kwargs={
+        # 'drop_countries': ["UA"]
+    },
+    case_alteration_kwargs={
+        'snapshot_i_range': slice(0, 24),
+        'use_unit_commitment': False,
+        'unit_commitment_path': "data/unit_commitment_halve_su_sd.csv",
+    }
+)  
 
 net_sep = pypsa.Network(get_input_networks_dir() / "pypsa-eur-ua-disconnected" / "nodal.nc")
 net_sep.buses.loc[:, 'zone_name'] = net_sep.buses.country
@@ -64,7 +64,7 @@ obj3 = main(
         # 'drop_countries': ["UA"]
     },
     case_alteration_kwargs={
-        'snapshot_i_range': slice(0, 2),
+        'snapshot_i_range': slice(0, 24),
         'use_unit_commitment': False,
         'unit_commitment_path': "data/unit_commitment_halve_su_sd.csv",
     }
