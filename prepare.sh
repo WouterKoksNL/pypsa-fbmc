934f50afc4158e0625ae8afc95669a0c7a38f7b2
WouterKoksNL <wouterkoks135@live.nl>
2026-05-20 11:27:42 +0200

    replace run by prepare (that doesnt run script)

--- prepare.sh
+++ prepare.sh
@@ -15,8 +15,7 @@ git pull
 # --- UPDATE PATHS ---
 INPUT_DIR="/mnt/beegfs/users/wouterko/fbmc/input"
 UNPROCESSED_DIR="/mnt/beegfs/users/wouterko/fbmc/input"
-
+RESULTS_DIR="/mnt/beegfs/users/wouterko/fbmc/results"
 sed -i "s|input_networks_dir = \".*\"|input_networks_dir = \"$INPUT_DIR\"|" paths.toml
 sed -i "s|unprocessed_input_networks_dir = \".*\"|unprocessed_input_networks_dir = \"$UNPROCESSED_DIR\"|" paths.toml
-
-python main.py
+sed -i "s|results_dir = \".*\"|results_dir = \"$RESULTS_DIR\"|" paths.toml
