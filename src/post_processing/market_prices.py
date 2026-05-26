fix calculation of market prices

src/post_processing/market_prices.py
@@ -31,8 +31,7 @@ def calculate_zonal_prices(zones, snapshots, z_ptdf_dict: dict[str], model: lp.M
         # zonal_balance_dual = model.constraints[f'Zonal_balance-subnet-{sub_network}'].dual
         cnec_upper_ram_dual = model.constraints[f'CNEC-upper-RAM-subnet-{sub_network}'].dual
         cnec_lower_ram_dual = model.constraints[f'CNEC-lower-RAM-subnet-{sub_network}'].dual
-        zonal_price = - slack_zone_dual_ser + (z_ptdf * cnec_upper_ram_dual).sum(dim='cnec') - (z_ptdf * cnec_lower_ram_dual).sum(dim='cnec')
-
+        zonal_price =- slack_zone_dual_ser + (z_ptdf * cnec_upper_ram_dual).sum(dim='cnec') + (z_ptdf * cnec_lower_ram_dual).sum(dim='cnec')
         zonal_prices_full.loc[:, subnet_zones] = zonal_price.to_pandas()
 
     return zonal_prices_full