f559e0379bb7ce76c8168386c8ae6b2622b30368
WouterKoksNL <wouterkoks135@live.nl>
2026-05-30 09:12:59 +0200

    add dual of NP limit to price calc

--- src/post_processing/market_prices.py
+++ src/post_processing/market_prices.py
@@ -1,8 +1,14 @@
 import linopy as lp
-import pandas as pd 
+import pandas as pd
 
 
-def calculate_zonal_prices(zones, snapshots, z_ptdf_dict: dict[str], model: lp.Model, sn_slack_zone_map: pd.Series) -> pd.DataFrame:
+def calculate_zonal_prices(
+    zones,
+    snapshots,
+    z_ptdf_dict: dict[str],
+    model: lp.Model,
+    sn_slack_zone_map: pd.Series,
+) -> pd.DataFrame:
     """Calculate zonal prices from the dual variables of the model constraints.
 
     Parameters
@@ -15,29 +21,48 @@ def calculate_zonal_prices(zones, snapshots, z_ptdf_dict: dict[str], model: lp.M
         A Series mapping sub_networks to their respective slack zones.
     Returns
     -------
-    pd.DataFrame       
+    pd.DataFrame
         A DataFrame with snapshots as index and zones as columns, containing the zonal prices.
 
     """
 
-    slack_zone_duals = model.constraints['Zone-definition'].dual.sel(Zone=sn_slack_zone_map.values)
+    slack_zone_duals = model.constraints["Zone-definition"].dual.sel(
+        Zone=sn_slack_zone_map.values
+    )
 
     zonal_prices_full = pd.DataFrame(index=snapshots, columns=zones)
-    
+
     for sub_network, z_ptdf in z_ptdf_dict.items():
-        slack_zone_dual_ser = slack_zone_duals.sel(Zone=sn_slack_zone_map[sub_network])  # timeseries 
-        subnet_zones = z_ptdf.coords['Zone'].values
+        slack_zone_dual_ser = slack_zone_duals.sel(
+            Zone=sn_slack_zone_map[sub_network]
+        )  # timeseries
+        subnet_zones = z_ptdf.coords["Zone"].values
         # Get the dual variables for the constraints
         # zonal_balance_dual = model.constraints[f'Zonal_balance-subnet-{sub_network}'].dual
-        cnec_upper_ram_dual = model.constraints[f'CNEC-upper-RAM-subnet-{sub_network}'].dual
+        cnec_upper_ram_dual = model.constraints[
+            f"CNEC-upper-RAM-subnet-{sub_network}"
+        ].dual
         cnec_lower_ram_dual = None
-        zonal_price = - slack_zone_dual_ser + (z_ptdf * cnec_upper_ram_dual).sum(dim='cnec')
-        if f'CNEC-lower-RAM-subnet-{sub_network}' in model.constraints:
-            cnec_lower_ram_dual = model.constraints[f'CNEC-lower-RAM-subnet-{sub_network}'].dual
-            zonal_price += (z_ptdf * cnec_lower_ram_dual).sum(dim='cnec')
+        zonal_price = -slack_zone_dual_ser + (z_ptdf * cnec_upper_ram_dual).sum(
+            dim="cnec"
+        )
+        if f"CNEC-lower-RAM-subnet-{sub_network}" in model.constraints:
+            cnec_lower_ram_dual = model.constraints[
+                f"CNEC-lower-RAM-subnet-{sub_network}"
+            ].dual
+            zonal_price += (z_ptdf * cnec_lower_ram_dual).sum(dim="cnec")
+
+        if "net_position_lower_limit_UA" in model.constraints:
+            lower_dual = model.constraints["net_position_lower_limit_UA"].dual
+            zonal_price.loc[lower_dual.snapshot, lower_dual.Zone] += lower_dual
+            breakpoint()
+
+        if "net_position_upper_limit_UA" in model.constraints:
+            upper_dual = model.constraints["net_position_upper_limit_UA"].dual
+            zonal_price.loc[upper_dual.snapshot, upper_dual.Zone] += upper_dual
+
         
-        
+
         zonal_prices_full.loc[:, subnet_zones] = zonal_price.to_pandas()
 
     return zonal_prices_full
-    
