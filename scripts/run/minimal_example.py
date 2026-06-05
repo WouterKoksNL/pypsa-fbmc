63b995f3cb0448156efdb4d77dfacb5729c4763c
WouterKoksNL <wouterkoks135@live.nl>
2026-06-05 09:51:14 +0200

    Update minimal_example.py

--- scripts/run/minimal_example.py
+++ scripts/run/minimal_example.py
@@ -1,16 +1,34 @@
 from pathlib import Path
 
 from fbmc.settings import FBMCConfig
-from fbmc.api import main
-from fbmc.case_creation.main import Cases
-
+from fbmc.api import main, run_fbmc
+from fbmc import Cases, create_case
+from fbmc.api import redispatch_workflow
 
 config_path = Path("config/base_config.yaml")
 config = FBMCConfig.from_base_yaml(config_path)
-obj = main(
-    save_path=Path("results/minimal_example"),
-    case_name=Cases.BASIC_THREE_NODE, 
+# obj = main(
+#     save_path=Path("results/minimal_example"),
+#     case_name=Cases.BASIC_THREE_NODE, 
+#     config=config,
+#     load_case_flag=False,
+# )  
+
+# print("Zonal market objective: ", obj)
+
+
+case_data = create_case(case=Cases.BASIC_THREE_NODE)
+
+fbmc_result = run_fbmc(
+    zonal_net=case_data['zonal_net'],
+    nodal_net=case_data['nodal_net'],
+    gsk=case_data['gsk_dict'],
     config=config,
-    load_case_flag=False,
-)  
-print("Zonal market objective: ", obj)
+)
+
+
+print(fbmc_result.net_positions)
+print(fbmc_result.zonal_net.model)
+
+
+
