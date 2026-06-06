ef8ada43d227c7cfccb36dfbe2d02a9e13a4466c
WouterKoksNL <wouterkoks135@live.nl>
2026-06-06 13:10:32 +0200

    Update __init__.py

---
+++ src/fbmc/case_creation/__init__.py
@@ -0,0 +1,10 @@
+
+from .main import Cases, create_case
+from .network_conversion import nodal_to_zonal, copy_net
+
+__all__ = [
+    "Cases",
+    "create_case",
+    "nodal_to_zonal",
+    "copy_net"
+]
