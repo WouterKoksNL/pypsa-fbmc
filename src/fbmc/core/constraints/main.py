53cef9678e7db734550e640fcf8147f24fba9027
WouterKoksNL <wouterkoks135@live.nl>
2026-06-06 15:18:49 +0200

    cleanup

--- src/fbmc/core/constraints/main.py
+++ src/fbmc/core/constraints/main.py
@@ -104,11 +104,6 @@ def remove_original_constraints(network):
         The zonal PyPSA network from which to remove constraints.
 
     """
-
-    # network.model.remove_variables("Link-p")
-    # network.model.remove_constraints("Link-fix-p-lower")
-    # network.model.remove_constraints("Link-fix-p-upper")
-    
     network.model.remove_constraints("Bus-nodal_balance")
     
     # if it exists, remove the bus-meshed-nodal_balance constraint as well.
