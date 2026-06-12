ddfe6c1b1f4e6f9f94a1f9577f8da96795d7f105
WouterKoksNL <wouterkoks135@live.nl>
2026-06-12 11:07:02 +0200

    set gsk strat to p_nom

--- scripts/run/minimal_example.py
+++ scripts/run/minimal_example.py
@@ -7,22 +7,13 @@ from fbmc.api import redispatch_workflow
 
 config_path = Path("config/base_config.yaml")
 config = FBMCConfig.from_base_yaml(config_path)
-# obj = main(
-#     save_path=Path("results/minimal_example"),
-#     case_name=Cases.BASIC_THREE_NODE, 
-#     config=config,
-#     load_case_flag=False,
-# )  
-
-# print("Zonal market objective: ", obj)
-
+config.gsk_strategy = "P_NOM"
 
 case_data = create_case(case=Cases.BASIC_THREE_NODE)
 
 fbmc_result = run_fbmc(
     zonal_net=case_data['zonal_net'],
     nodal_net=case_data['nodal_net'],
-    gsk=case_data['gsk_dict'],
     config=config,
 )
 
