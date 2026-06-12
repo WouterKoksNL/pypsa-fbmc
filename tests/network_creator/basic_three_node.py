ab32892284e194c5dab9d4ae7fd9f6d557135c66
WouterKoksNL <wouterkoks135@live.nl>
2026-06-12 17:00:50 +0200

    make network creator inside tests

---
+++ tests/network_creator/basic_three_node.py
@@ -0,0 +1,37 @@
+import pypsa
+import pandas as pd
+
+from fbmc.input_network_conversions.network_conversion import nodal_to_zonal
+
+def create_basic_three_node_case():
+    # three-node network with one zone having two nodes
+    nodal_net = pypsa.Network()
+    nodal_net.set_snapshots(['1', '2'])
+    nodal_net.add('Bus', ['A1', 'B1', 'B2'])
+    nodal_net.buses.loc[:, 'zone_name'] = ['A', 'B', 'B']
+    nodal_net.add('Line', 'B1-A1', bus0='B1', bus1='A1', x=1, s_nom=12)
+    nodal_net.add('Line', 'B1-B2', bus0='B1', bus1='B2', x=1, s_nom=12)
+    nodal_net.add('Line', 'A1-B2', bus0='A1', bus1='B2', x=1, s_nom=12)
+
+    nodal_net.add('Generator', 'gen_A1', bus='A1', p_nom=100, marginal_cost=400, carrier="Oil")
+    nodal_net.add('Generator', 'gen_B1', bus='B1', p_nom=100, marginal_cost=100, carrier="Wind")
+    nodal_net.add('Generator', 'gen_B2', bus='B2', p_nom=100, marginal_cost=200, carrier="CCGT")
+    nodal_net.add('Load', 'load_A1', bus='A1', p_set=[15, 15])
+
+    zonal_net = nodal_to_zonal(nodal_net.copy(), nodal_net.buses.zone_name)
+    zonal_net.remove('Link', zonal_net.links.index)
+    # nodal_net.optimize(solver_name='gurobi')
+
+    gsk = pd.DataFrame(0., index=zonal_net.buses.index, columns=nodal_net.buses.index)
+    gsk.loc['A', 'A1'] = 1.0
+    gsk.loc['B', 'B1'] = 1.
+    gsk.loc['B', 'B2'] = 0.0
+    gsk.columns.name = "Bus"
+    gsk.index.name = "Zone"
+    gsk_dict = {snapshot: gsk.copy() for snapshot in zonal_net.snapshots}
+    output = {
+        'zonal_net': zonal_net,
+        'nodal_net': nodal_net,
+        'gsk_dict': gsk_dict
+    }
+    return output
