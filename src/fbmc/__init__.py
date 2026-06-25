bb00854d81262e8cefab100a36cf16d3e3097eda
WouterKoksNL <wouterkoks135@live.nl>
2026-06-25 15:36:09 +0200

    simplify package imports

--- src/fbmc/__init__.py
+++ src/fbmc/__init__.py
@@ -3,9 +3,11 @@
 from . import accessor as accessor  # registers pypsa.Network.fbmc on import
 from .api import run_fbmc
 from .input_network_conversions.network_conversion import nodal_to_zonal
-from .settings import FBMCConfig, merge_config_overrides
-from .enums import GSKStrategy, BaseCaseStrategy
-
+from .settings import FBMCConfig as FBMCConfig
+from .settings import merge_config_overrides
+from .enums import GSKStrategy as GSKStrategy
+from .enums import BaseCaseStrategy as BaseCaseStrategy
+from .enums import CNECStrategy as CNECStrategy
 from .types import FBMCResult, DispatchResult
 
 __all__ = [
